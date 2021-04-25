807c2045200a4efdeb6b547d8e5190cb67a51e02
Idowu Hayub <idowuolatunji3@gmail.com>
2021-04-25 07:39:40 +0100

    Lesson and quiz schema

---
+++ SchAppAPI/Migrations/20210425063836_lessonreport2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchAppAPI.Migrations
+{
+    public partial class lessonreport2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- SchAppAPI/Models/Lesson/Quiz.cs
+++ SchAppAPI/Models/Lesson/Quiz.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace SchAppAPI.Models
+namespace SchAppAPI.Models.Lesson
 {
     public class Quiz : BaseEntity
     {
