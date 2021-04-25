90aa459c28a302070dc3e5ae1c754e5e8b692938
Idowu Hayub <idowuolatunji3@gmail.com>
2021-04-25 21:06:38 +0100

    Migration

--- SchAppAPI/Migrations/20210425194210_LessonSchemaUpdate.Designer.cs
+++ SchAppAPI/Migrations/20210425194210_LessonSchemaUpdate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchAppAPI.Contexts;
 
 namespace SchAppAPI.Migrations
 {
     [DbContext(typeof(SchoolDbContext))]
-    partial class SchoolDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210425194210_LessonSchemaUpdate")]
+    partial class LessonSchemaUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SchAppAPI/Migrations/20210425194210_LessonSchemaUpdate.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchAppAPI.Migrations
+{
+    public partial class LessonSchemaUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_Contents_ContentId",
+                table: "Lessons");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_Quizzes_QuizId",
+                table: "Lessons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Lessons_ContentId",
+                table: "Lessons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Lessons_QuizId",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "ContentId",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "QuizId",
+                table: "Lessons");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LessonId",
+                table: "Quizzes",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LessonId",
+                table: "Contents",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quizzes_LessonId",
+                table: "Quizzes",
+                column: "LessonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_LessonId",
+                table: "Contents",
+                column: "LessonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contents_Lessons_LessonId",
+                table: "Contents",
+                column: "LessonId",
+                principalTable: "Lessons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Quizzes_Lessons_LessonId",
+                table: "Quizzes",
+                column: "LessonId",
+                principalTable: "Lessons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contents_Lessons_LessonId",
+                table: "Contents");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Quizzes_Lessons_LessonId",
+                table: "Quizzes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Quizzes_LessonId",
+                table: "Quizzes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contents_LessonId",
+                table: "Contents");
+
+            migrationBuilder.DropColumn(
+                name: "LessonId",
+                table: "Quizzes");
+
+            migrationBuilder.DropColumn(
+                name: "LessonId",
+                table: "Contents");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ContentId",
+                table: "Lessons",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "QuizId",
+                table: "Lessons",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_ContentId",
+                table: "Lessons",
+                column: "ContentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_QuizId",
+                table: "Lessons",
+                column: "QuizId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_Contents_ContentId",
+                table: "Lessons",
+                column: "ContentId",
+                principalTable: "Contents",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_Quizzes_QuizId",
+                table: "Lessons",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
