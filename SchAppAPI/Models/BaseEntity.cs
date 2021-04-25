6aecbd2bd82290a0e8be703d0796d6f09bcf9cbf
horbarz <horbarz007@gmail.com>
2021-04-25 05:00:28 +0100

    Added lesson management changes

--- SchAppAPI/Models/BaseEntity.cs
+++ SchAppAPI/Models/BaseEntity.cs
@@ -9,7 +9,7 @@ namespace SchAppAPI.Models
         public Guid Id { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
-        public bool IsActive { get; set; }
-        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; } = false;
     }
 }
